70ed09ca04b2fe39d7adec96df8b3948841006f3
luiscacostas <acosta.luisc15@gmail.com>
2024-07-19 06:05:35 +0200

    add render deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://proyecto-final-the-bridge.onrender.com', // Cambia esto a la URL de tu backend en Render
+    },
+  },
 })
