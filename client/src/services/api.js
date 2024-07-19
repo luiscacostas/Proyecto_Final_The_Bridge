29e4a48ad068696299e69e834adfc8a985256e1d
luiscacostas <acosta.luisc15@gmail.com>
2024-07-19 08:01:49 +0200

    add changes i api-url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://proyecto-final-the-bridge.onrender.com/';
 
 export const register = async (userData) => {
   const response = await fetch(`${API_URL}/auth/register`, {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://proyecto-final-the-bridge.onrender.com', // Cambia esto a la URL de tu backend en Render
+      '/api': 'https://proyecto-final-the-bridge.onrender.com',
     },
   },
 })
