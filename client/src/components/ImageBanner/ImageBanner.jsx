7ed51a88ff12299fca205d1ddabb4e761a885f27
luiscacostas <acosta.luisc15@gmail.com>
2024-07-21 23:25:02 +0200

    add changes in images

--- client/src/components/ImageBanner/ImageBanner.jsx
+++ client/src/components/ImageBanner/ImageBanner.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import imagen1 from '../../assets/banner5.webp';
-import imagen2 from '../../assets/banner6.webp';
 import imagen3 from '../../assets/banner4.webp';
-import imagen4 from '../../assets/imagen1.jpeg';
+
   
 const ImageBanner = () => {
       const images = [
           imagen1,
-          imagen2,
-          imagen3,
-          imagen4
+          imagen3
       ];
 
     return (
